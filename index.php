f495fa4a30d62a2bb9faca0922a0d3f0a3039070
Shavit Netzer <shavit.netzer@gmail.com>
2014-11-12 16:12:42 +0200

    delete all

--- index.php
+++ index.php
@@ -1,44 +1,50 @@
-<!DOCTYPE html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>shanet</title>
-    <link rel="stylesheet" type="text/css" href="jquery.mobile-1.4.5.min.css">
-    <script type="text/javascript" charset="utf-8" src="jquery_1.5.2.min.js"></script>
-    <script type="text/javascript" charset="utf-8" src="jquery.mobile-1.4.5.min.js"></script>
-    <script type="text/javascript" charset="utf-8" src="ShaNet.js"></script>
-</head>
-<body>
-<form>
-  <p>
-  <label><label>Please Fill up the form and submit.<br />
-  </label>Name
-  <input type="text" name="name" id="name" placeholder="Please Input Name" />
-    </label>
-  </p>
-  <p>
-    <label>Age
-        <input type="number" name="age" value="25" id="age" placeholder="Please Input Age" />
-    </label>
-  </p>
-  <p>
-    <label>
-      <input type="button" name="submit" id="submit" value="Submitush" />
-    </label>
-  </p>
-
-</form>
-  <?php
-   class Emp {
-       public $name = "";
-       public $hobbies  = "";
-       public $birthdate = "";
-   }
-   $e = new Emp();
-   $e->name = "sachin";
-   $e->hobbies  = "sports";
-   $e->birthdate = date('m/d/Y h:i:s a', strtotime("8/5/1974 12:20:03"));
-
-   echo json_encode($e);
-?>
-</body>
-</html>
+<?php
+// Define path to data folder
+define('DATA_PATH', realpath(dirname(__FILE__).'/data'));
+ 
+//include our models
+include_once 'models/TodoItem.php';
+ 
+//wrap the whole thing in a try-catch block to catch any wayward exceptions!
+try {
+    //get all of the parameters in the POST/GET request
+    $params = $_REQUEST;
+     
+    //get the controller and format it correctly so the first
+    //letter is always capitalized
+    $controller = ucfirst(strtolower($params['controller']));
+     
+    //get the action and format it correctly so all the
+    //letters are not capitalized, and append 'Action'
+    $action = strtolower($params['action']).'Action';
+ 
+    //check if the controller exists. if not, throw an exception
+    if( file_exists("controllers/{$controller}.php") ) {
+        include_once "controllers/{$controller}.php";
+    } else {
+        throw new Exception('Controller is invalid.');
+    }
+     
+    //create a new instance of the controller, and pass
+    //it the parameters from the request
+    $controller = new $controller($params);
+     
+    //check if the action exists in the controller. if not, throw an exception.
+    if( method_exists($controller, $action) === false ) {
+        throw new Exception('Action is invalid.');
+    }
+     
+    //execute the action
+    $result['data'] = $controller->$action();
+    $result['success'] = true;
+     
+} catch( Exception $e ) {
+    //catch any exceptions and report the problem
+    $result = array();
+    $result['success'] = false;
+    $result['errormsg'] = $e->getMessage();
+}
+ 
+//echo the result of the API call
+echo json_encode($result);
+exit();
