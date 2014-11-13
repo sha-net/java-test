3ac79bad04b89c68865da423a22f8e8441f690b8
Shavit Netzer <shavit.netzer@gmail.com>
2014-11-13 20:30:25 +0200

    sdfdfs

--- index.php
+++ index.php
@@ -3,4 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {echo "POST";}
 if ($_SERVER["REQUEST_METHOD"] == "PUT") {echo "PUT";}
 if ($_SERVER["REQUEST_METHOD"] == "GET") {echo "GET";}
 if ($_SERVER["REQUEST_METHOD"] == "DELETE") {echo "DELETE";}
+$request_body = file_get_contents('php://input');
+$json = json_decode($request_body);
+print_r($json);
 ?>
