6562a0770d4580fa7a07ab89129e9efccba66ca3
Shavit Netzer <shavit.netzer@gmail.com>
2014-11-16 22:16:22 +0200

    Git auto update

--- index.php
+++ index.php
@@ -1,39 +1,15 @@
 <?php
-getFromDB();
+require 'index-faunctions.php';
+
 $arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {echo "POST";}
 if ($_SERVER["REQUEST_METHOD"] == "PUT") {echo "PUT";}
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-	echo "GET";
-	if($_GET["ID"] == "3450"){
-		echo json_encode($arr);
-		echo "OK";
-		print_r($arr);
-	}else{
-		echo "sdfsdf";
-	}
-	getFromDB();
-}
+if ($_SERVER["REQUEST_METHOD"] == "GET") {echo "GET";verifyGetRequest("3450");}
 if ($_SERVER["REQUEST_METHOD"] == "DELETE") {echo "DELETE";}
+
+echo "Now i dont know - Start";
 $request_body = file_get_contents('php://input');
 $json = json_decode($request_body);
 print_r($json);
-
-
-#################### Functions #######################
-function getFromDB(){
-	$dbconn = pg_connect("host=localhost dbname=apiDB user=apiDB password=apiDB")
-	    or die('Could not connect: ' . pg_last_error());
-	// Performing SQL query
-	$query = 'SELECT * FROM book';
-	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
-	$data = pg_fetch_array($result, NULL, PGSQL_ASSOC);
-	print_r($data);
-	// Free resultset
-	pg_free_result($result);	
-	// Closing connection
-	pg_close($dbconn);
-	
-}
-
+echo "Now i dont know - Finish";
 ?>
