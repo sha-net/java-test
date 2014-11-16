beea0f84f0bd30901248e78466a3591e35e32a3f
Shavit Netzer <shavit.netzer@gmail.com>
2014-11-16 22:16:28 +0200

    Git auto update

---
+++ index-faunctions.php
@@ -0,0 +1,24 @@
+function getFromDB(){
+	$dbconn = pg_connect("host=localhost dbname=apiDB user=apiDB password=apiDB")
+	    or die('Could not connect: ' . pg_last_error());
+	// Performing SQL query
+	$query = 'SELECT * FROM book';
+	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+	$data = pg_fetch_array($result, NULL, PGSQL_ASSOC);
+	print_r($data);
+	// Free resultset
+	pg_free_result($result);	
+	// Closing connection
+	pg_close($dbconn);	
+}
+
+function verifyGetRequest($id){
+	if($_GET["ID"] == $id){
+		echo "If";
+		echo json_encode($arr);
+		echo "OK";
+		print_r($arr);
+	}else{
+		echo "Else";
+	}
+}
