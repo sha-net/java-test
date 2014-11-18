6d2f0f96b622263b2335c1bbebd3837e205e803a
Shavit Netzer <shavit.netzer@gmail.com>
2014-11-18 12:58:39 +0200

    Git auto update

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ echo "Now i dont know - Start";
 $request_body = file_get_contents('php://input');
 $json = json_decode($request_body);
 print_r($json);
+print_r($request_body);
 error_log($json . "test" . $request_body);
 //print_r($arr);
 echo "Now i dont know - Finish";
